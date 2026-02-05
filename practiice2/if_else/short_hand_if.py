d62b9eac6962a85d263599fd181e688f874d6563
Zainitdin <zainitdin4@gmail.com>
2026-02-06 00:59:23 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ practiice2/if_else/short_hand_if.py
@@ -0,0 +1,7 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+c = 2
+d = 330
+print("A") if c > d else print("B")
---
+++ practiice2/loops/for_loop.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+for b in "banana":
+  print(b)
