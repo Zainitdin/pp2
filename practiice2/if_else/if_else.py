2f1b8ea541fb493a8ad10b38e03fa7799ccd260d
Zainitdin <zainitdin4@gmail.com>
2026-02-19 21:20:00 +0500

    Check

--- practiice2/if_else/if_else.py
+++ practiice2/if_else/if_else.py
@@ -1,8 +1,5 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
+number = 7
+if number % 2 == 0:
+    print("The number is even")
+else: 
+    print("The number is odd")
